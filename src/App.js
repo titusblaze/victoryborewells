989a3097b4aca1a0e1a8495e2095aebb5df90b5d
titus <titus.blaze@gmail.com>
2025-06-18 21:39:58 +0530

    titus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './component/Navbar';
-import { Banner } from './component/Banner';
 import { Body2 } from './component/Body2';
 import { BodyOne } from './component/BodyOne';
 import Footer from './component/Footer';
-import MobNav from './component/MobNav';
-import OfferMobile from './component/OfferMobile';
-import BodyFour from './component/BodyFour';
-import BodyFive from './component/BodyFive';
-import BodySix from './component/BodySix';
-import Body3 from './component/Body3';
 import ScrollupIcon from './component/ScrollupIcon';
-import Offer from './component/Offer';
-import Example1 from './component/Example1';
-import Example from './component/Example';
 import Example2 from './component/Example2';
-import GoogleSheet from './component/GoogleSheet';
-import Example3 from './component/Example3'; 
-import NewsScroller from './component/NewsScroller';
-import RunningNumber from './component/RunningNumber';
 import AboutUs from './component/AboutUs';
 import AutoNumberRunning from './AutoNumberRunning';
 import ImageSlider from './component/ImageSlider';
@@ -30,7 +16,6 @@ function App() {
     <div className="App">
       <Navbar/>
       <ImageSlider/>
-      {/* <Banner/> */}
       <AboutUs/>
       <BodyOne/>
       <Body2/>
@@ -38,18 +23,7 @@ function App() {
       <AutoNumberRunning/>
       <ScrollupIcon/> 
       <Footer/>
-      {/* <Body3/>  */}
-      {/* <BodyFour/> */}
-      {/* <BodyFive/> */}
-      {/* <BodySix/> */}
-      {/* <Example/> */}
-      {/* <BodyOne/> */}
-      {/* <Example3/> */}
-      {/* <RunningNumber/> */}
-      {/* <GoogleSheet/>  */}
-      {/* <Example1/>
-       <Offer/>
-      <OfferMobile/> */}
+      
       
       
       
